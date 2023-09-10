9b594b9da8a4ff7c37ac97205af4d8151a7c7335
minkonging <skysmksky@gmail.com>
2023-09-10 17:14:07 +0900

    line width controll complete

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
 
 function onMove(event){
+    if (isPainting) {
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+    }
     ctx.moveTo(event.offsetX, event.offsetY);
 }
-function onMouseDown(){
+function startPainting(){
     isPainting = true;
 }
-function onMouseUp() {
+function cancelPainting() {
     isPainting = false;
+    ctx.beginPath();
+}
+function onLineWidthChange(event){
+    ctx.lineWidth = event.target.value;
 }
 
 canvas.addEventListener("mousemove", onMove);
-canvas.addEventListener("mousedown", onMouseDown);
-canvas.addEventListener("mouseup", onMouseUp);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange);
